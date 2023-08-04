165fa1685e0085f17343522553fb1328225abfae
Sianpiang <sianpiang8@gmail.com>
2023-08-05 00:30:51 +0530

    phonebook 2.6 complete

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,20 +6,29 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
-
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setPersons(persons.concat({name:newName}))
+    setNewName("")
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={(e)=>setNewName(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person=>(
+        <p key={person.name}>Name: {person.name}</p>
+      ))}
     </div>
   )
 }
