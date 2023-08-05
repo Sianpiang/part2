ef71ebaeb231fc0bfaf56ec20537624b57dc7098
Sianpiang <sianpiang8@gmail.com>
2023-08-06 00:42:48 +0530

    add fetch from json server

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,28 +1,32 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 import Display from './components/Display'
 import Search from './components/Search'
 import Form from './components/Form'
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [search, setSearch] = useState('')
+  useEffect(()=>{
+    const fetchPersons=async()=>{
+      const getPersons=await axios.get("http://localhost:3001/persons");
+      setPersons(getPersons.data)
+    }
+    fetchPersons();
+  },[])
   const handleSubmit = (e)=>{
     e.preventDefault();
-    console.log(persons[0].name.toLowerCase())
     const Isthere = persons.find((person)=>person.name===newName?true:false);
     if(!Isthere){
     setPersons(persons.concat({name:newName,number:newNum}))
     setNewName("")
+    setNewNum("")
     }
     else{
       alert(`${newName} is already added to phonebook`)
       setNewName("")
+      setNewNum("")
     }
   }
   return (
